07dc6ca9f132269831ae90119366733598fa5a6c
Jmukumzoda <jmukumzoda63@gmail.com>
2023-10-30 14:06:56 +0500

    first commit

---
+++ js/db.js
@@ -0,0 +1,25 @@
+let arr = [
+    {
+        id:1232312,
+        cart_name: "Visa",
+        cart_amount: "Rub",
+        cart_Category: "Car",
+        cart_Transaction_amount: "414,000,000",
+        cart_time: "4 days ago",
+    },
+    {
+        id:1232312,
+        cart_name: "Visa",
+        cart_amount: "Rub",
+        cart_Category: "Car",
+        cart_Transaction_amount: "414,000,000",
+        cart_time: "4 days ago",
+    }, {
+        id:1232312,
+        cart_name: "Visa",
+        cart_amount: "Rub",
+        cart_Category: "Car",
+        cart_Transaction_amount: "414,000,000",
+        cart_time: "4 days ago",
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,47 @@
+let container = document.querySelector('.cart')
+let table = document.querySelector('.tablet')
+reload(arr)
+function reload(massiv) {
+    container.innerHTML = ""
+
+    for (let item of massiv) {
+        let article = document.createElement('article'),
+            h3 = document.createElement('h3'),
+            p = document.createElement('p'),
+            tablet = document.createElement('table'),
+            tr = document.createElement('tr'),
+            th = document.createElement('th'),
+            p_table = document.createElement('p'),
+            p_name = document.createElement('p'),
+            p_car = document.createElement('p'),
+            p_tom = document.createElement('p');
+
+
+
+
+
+        article.classList.add('grid')
+        article.style.background=" linear-gradient(84deg, #5F0A87 2.27%, #A4508B 92.26%)"
+        tr.classList.add('tr_reload')
+        th.classList.add('th')
+        p.classList.add('grid_p')
+
+        h3.innerHTML = item.cart_name
+        p.innerHTML = item.cart_amount
+        th.innerHTML = item.id
+        p_table.innerHTML = item.cart_name
+        p_name.innerHTML = item.cart_amount
+        p_car.innerHTML = item.cart_Transaction_amount
+        p_tom.innerHTML = item.cart_time
+
+
+
+
+        container.append(article)
+        article.append(h3, p)
+        table.append(tablet)
+        tablet.append(tr)
+        tr.append(th, p_table, p_car, p_name, p_tom)
+
+    }
+}
